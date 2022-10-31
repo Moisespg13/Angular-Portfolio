888ae95b929833673d5ffb270b56143b6bdc3318
Ramsés <Ramss@example.com>
2022-10-31 14:37:26 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FootComponent } from './foot/foot.component';
+import { AboutComponent } from './about/about.component';
+import { EduComponent } from './edu/edu.component';
+import { ExpComponent } from './exp/exp.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FootComponent,
+    AboutComponent,
+    EduComponent,
+    ExpComponent,
+    ProjectsComponent,
+    SkillsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
